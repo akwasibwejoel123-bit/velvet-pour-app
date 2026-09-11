835593bc2caaa129f2c35df48cb20902ddca05cf
Akwasibwe joel <akwasibwejoel123@icloud.com>
2026-09-11 22:24:47 +0300

    Create Home component for Velvet Pour page

---
+++ app/page.js
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Velvet Pour</h1>
+      <p>Premium cocktails. Delivered to you.</p>
+
+      <button>Order Now</button>
+    </main>
+  );
+}
